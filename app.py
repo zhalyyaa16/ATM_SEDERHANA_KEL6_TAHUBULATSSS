b64ccf2d4f35306d169fddf237f35ea770275f1b
zhalyyaa16 <arianizhafira06@gmail.com>
2026-06-14 00:22:38 +0700

    Menambahkan fitur login

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from login import login
+from data import akun
+
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+if "user" not in st.session_state:
+    st.session_state.user = None
+
+st.title("BCI")
+st.subheader("Mobile Banking")
+
+no_rekening = st.text_input("Masukkan nomor rekening: ")
+pin = st.text_input("Masukkan PIN: ", type="password")
+
+if st.button("Login"):
+    user = login(no_rekening, pin)
+    if user:
+        st.session_state.login = True
+        st.session_state.user = user
+
+        st.success(f"Login berhasil! Selamat datang, {akun[user]['nama']}!")
+    else:
+        st.error("Nomor rekening atau PIN salah. Silakan coba lagi.")
+
+if st.session_state.login:
+    st.write("Anda sudah login!")
---
+++ login.py
@@ -0,0 +1,8 @@
+from data import akun
+
+def login(no_rekening, pin):
+
+    if no_rekening in akun and akun[no_rekening]["pin"] == pin:
+        return no_rekening
+
+    return None
