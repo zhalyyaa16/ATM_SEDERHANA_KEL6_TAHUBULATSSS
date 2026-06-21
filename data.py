472e282f09994dfeb334861544d84be6ed197b35
zhalyyaa16 <arianizhafira06@gmail.com>
2026-06-22 02:45:32 +0700

    ubah data jd listt

--- data.py
+++ data.py
@@ -1,13 +1,14 @@
 import csv
 
-akun = {}
+akun = []
 
-with open("data.csv", newline="") as file:
+with open("data.csv", newline="", encoding="utf-8") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
-        akun[row["no_rekening"]] = {
+        akun.append({
+            "no_rekening": row["no_rekening"],
             "nama": row["nama"],
             "pin": row["pin"],
             "saldo": int(row["saldo"])
-        }
+        })
